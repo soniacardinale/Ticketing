51ff74c10eb5ea857928c79f1f3d136169844241
Sonia Cardinale <sonia.cardinale@avanade.com>
2020-12-01 14:40:55 +0100

    v 1.2

---
+++ Ticketing.Client/Migrations/20201201133440_NotesAdded.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Ticketing.Client.Migrations
+{
+    public partial class NotesAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Notes",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Comments = table.Column<string>(maxLength: 1000, nullable: false),
+                    TicketID = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Notes", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Ticket_Notes",
+                        column: x => x.TicketID,
+                        principalTable: "Tickets",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Notes_TicketID",
+                table: "Notes",
+                column: "TicketID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Notes");
+        }
+    }
+}
---
+++ Ticketing.Client/Model/Configuration/TicketConfiguration.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ticketing.Client.Model.Configuration
+{
+    public class TicketConfiguration : IEntityTypeConfiguration<Ticket>
+    {
+        public void Configure(EntityTypeBuilder<Ticket> builder)
+        {
+            //Fluent API (in ticket utilizzo la Data Notation, ma la coomento perchè altrimenti farei 2 volte la stessa cosa)
+
+            //Anzichè scrivere ogni volta il modelBUilder come segue:
+            //modelBuilder.Entity<Ticket>()
+            //    .HasKey(t => t.ID); //non è necessario perchè abbiamo rispettato la Data Notation, però meglio specificare
+            //modelBuilder.Entity<Ticket>()
+            //    .Property(t => t.Titolo)
+            //    .HasMaxLength(100) //ho imposto la lunghezza massima del titolo di 100 caratteri
+
+            //Definiamo:
+            //var ticketModel = modelBuilder.Entity<Ticket>();
+
+            //E le proprietà le scriviamo:
+            builder
+                .HasKey(t => t.ID);
+
+            builder
+                .Property(t => t.Titolo)
+                .HasMaxLength(100);
+
+            builder
+             .Property(t => t.Description)
+             .HasMaxLength(500);
+
+            builder
+                .Property(t => t.Priority)
+                .IsRequired(); //sto richiedendo che sia obbligatoria, non può essere nulla
+
+            builder
+                .Property(t => t.Requestor)
+                .HasMaxLength(50)
+                .IsRequired();
+
+            builder
+                .HasMany(t => t.Notes) //Relazione tra Ticket e Notes: ogni Ticket può avere più note
+                .WithOne(n => n.Ticket) //Ogni nota va ad un solo ticket
+                .HasForeignKey(n => n.TicketID)
+                .HasConstraintName("FK_Ticket_Notes")
+                .OnDelete(DeleteBehavior.Cascade);  //La relazione viene definita in un solo posto, non serve ridefinirla in Note
+            
+
+
+
+
+        }
+    }
+}
+
