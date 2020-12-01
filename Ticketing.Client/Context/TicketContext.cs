75e3b655472bfa4bbbaa9be1c1204dcca820aacf
Sonia Cardinale <sonia.cardinale@avanade.com>
2020-12-01 10:08:44 +0100

    Add project files.

---
+++ Ticketing.Client/Context/TicketContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Ticketing.Client.Model;
+
+namespace Ticketing.Client.Context
+{
+    public class TicketContext : DbContext
+    {
+        DbSet<Ticket> Ticketts { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionBuilder)
+        {
+            var config = new ConfigurationBuilder() //legge informazioni da diverse fonti
+                .SetBasePath(Directory.GetCurrentDirectory()) //forzo il configurationbuilder a cercare il file nella Current Directory
+                .AddJsonFile("apsettings.json")  //vado a prendere le informazioni nel mio file appsettings
+                .Build();
+
+            string connString=config.GetConnectionString("TicketDb");
+            //Oppure:
+            //string connString = config.GetConnectionString("ConnectionString")["TicketDb"];
+            //in questo modo la connection string sarà un file esterno 
+
+            optionBuilder.UseSqlServer("connString"); 
+        }
+    }
+}
---
+++ Ticketing.Client/Model/Ticket.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ticketing.Client.Model
+{
+    public class Ticket
+    {
+        public int ID { get; set; } //posso mettere o TicketID o solo ID
+        public DateTime IssueDate { get; set; }
+        public string Titolo { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public string  Priority { get; set; }
+        public string State { get; set; }
+    }
+}
