e8536a79859e8ca3c910fb5279cbef6bce8fccc9
Sonia Cardinale <sonia.cardinale@avanade.com>
2020-12-01 14:48:51 +0100

    Revert "Revert "v 1.2""
    This reverts commit 30f3452df75e48cfff24cd5ac23345336d506e2c.

---
+++ Ticketing.Client/Model/Note.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ticketing.Client.Model
+{
+    public class Note
+    {
+        public int ID { get; set; }
+        public string Comments { get; set; }
+        public int TicketID { get; set; }  //Foreign Key => Ticket
+        public virtual Ticket Ticket { get; set; }
+    }
+}
